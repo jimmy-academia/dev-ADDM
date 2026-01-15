1cef924d770a073834fbe447673a2b987ffbb409
Jimmy Yeh <marrch30@gmail.com>
2026-01-15 15:41:27 -0600

    just another push for progress

--- src/addm/eval/__init__.py
+++ src/addm/eval/__init__.py
@@ -1,1 +1,25 @@
-"""Package."""
+"""Evaluation utilities."""
+
+from addm.eval.auprc import (
+    CLASS_ORDER,
+    CLASS_NAMES,
+    DEFAULT_TOLERANCES_V1,
+    DEFAULT_TOLERANCES_V2,
+    compute_avg_primitive_accuracy,
+    compute_primitive_accuracy,
+    calculate_ordinal_auprc,
+    calculate_from_results_file,
+    print_report,
+)
+
+__all__ = [
+    "CLASS_ORDER",
+    "CLASS_NAMES",
+    "DEFAULT_TOLERANCES_V1",
+    "DEFAULT_TOLERANCES_V2",
+    "compute_avg_primitive_accuracy",
+    "compute_primitive_accuracy",
+    "calculate_ordinal_auprc",
+    "calculate_from_results_file",
+    "print_report",
+]
